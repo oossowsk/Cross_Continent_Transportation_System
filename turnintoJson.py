ef395cc3fcc2dbfeb422d4a07f8d038a490932be
Hector-Salermo <84691943+Hector-Salermo@users.noreply.github.com>
2023-04-12 15:41:48 -0400

    updated to have more travel paths

--- turnintoJson.py
+++ turnintoJson.py
@@ -4,91 +4,112 @@ import sys
 import re
 
 Location1 = {
-  "LocationID": 1,
+  "LocationID": 0,
   "City": "New York City",
   "Country": "United States",
   "Continent": "North America"
 }
 Location2 = {
-  "LocationID": 2,
+  "LocationID": 1,
   "City": "Tokyo",
   "Country": "Japan",
   "Continent": "Asia"
 }
 Location3 = {
-  "LocationID": 3,
+  "LocationID": 2,
   "City": "London",
   "Country": "United Kingdom",
   "Continent": "Europe"
 }
 Location4 = {
-  "LocationID": 4,
+  "LocationID": 3,
   "City": "Los Angeles",
   "Country": "United States",
   "Continent": "North America"
 }
 Location5 = {
-  "LocationID": 5,
+  "LocationID": 4,
   "City": "Cairo",
   "Country": "Egypt",
   "Continent": "Africa"
 }
 Location6 = {
-  "LocationID": 6,
+  "LocationID": 5,
   "City": "Paris",
   "Country": "France",
   "Continent": "Europe"
 }
-node1 ={
+node0 ={
+  "nodeNum": 0
+}
+node1 = {
   "nodeNum": 1
 }
-node2 = {
+node2= {
   "nodeNum": 2
 }
-node3= {
+node3 ={
   "nodeNum": 3
 }
-node4 ={
+node4 = {
   "nodeNum": 4
 }
-node5 = {
+node5 ={
   "nodeNum": 5
 }
-node6 ={
-  "nodeNum": 6
+nodepaths0 ={
+  "nodeNum": 0,
+  "endNode": 2,
+  "pathTime": 1468800,
+  "pathWeight": 110000000,
+  "pathType": "Ship"
 }
-nodepaths1 ={
-  "nodeNum": 1,
-  "endNode": 3,
+nodepaths1 = {
+  "nodeNum": 0,
+  "endNode": 2,
   "pathTime": 1468800,
   "pathWeight": 110000000,
   "pathType": "Ship"
 }
 nodepaths2 = {
-  "nodeNum": 1,
-  "endNode": 3,
-  "pathTime": 1468800,
-  "pathWeight": 110000000,
-  "pathType": "Ship"
-}
-nodepaths3 = {
-  "nodeNum": 3,
-  "endNode": 6,
+  "nodeNum": 2,
+  "endNode": 5,
   "pathTime": 4500,
   "pathWeight": 5000,
   "pathType": "Plane"
 }
-nodepaths4= {
-  "nodeNum": 2,
-  "endNode": 5,
+nodepaths3= {
+  "nodeNum": 1,
+  "endNode": 4,
   "pathTime": 1468800,
   "pathWeight": 110000000,
   "pathType": "Ship"
 }
-nodepaths5 ={
-  "nodeNum": 1,
-  "endNode": 4,
+nodepaths4 ={
+  "nodeNum": 0,
+  "endNode": 3,
   "pathTime": 281700,
   "pathWeight": 12500000,
   "pathType": "Train"
+}
+nodepaths5 ={
+  "nodeNum": 3,
+  "endNode": 1,
+  "pathTime": 950400,
+  "pathWeight": 110000000,
+  "pathType": "Ship"
+}
+nodepaths6 ={
+  "nodeNum": 4,
+  "endNode": 5,
+  "pathTime": 17100,
+  "pathWeight": 5000,
+  "pathType": "Plane"
+}
+nodepaths7 ={
+  "nodeNum": 5,
+  "endNode": 1,
+  "pathTime": 29700,
+  "pathWeight": 5000,
+  "pathType": "Plane"
 }
